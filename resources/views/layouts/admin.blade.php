e04122c580b184664a9650265819415d337b99b0
Vyacheslav Oleshko <vyacheslav.oleshko.work@gmail.com>
2021-07-03 19:07:51 +0300

    layout update

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -15,14 +15,16 @@
         <li class="nav-item">
           <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
         </li>
+      </ul>
+
+      <ul class="navbar-nav ml-auto">
         <li class="nav-item d-none d-sm-inline-block">
           <a href="{{ route('logout') }}" class="nav-link" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">{{ __('Logout') }}</a>
           <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
             @csrf
           </form>
         </li>
-      </ul>
-      <!-- Right navbar links -->
+    </ul>
     </nav>
     <!-- /.navbar -->
 
