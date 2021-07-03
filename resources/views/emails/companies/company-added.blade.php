05d9c2c6309f764dee0297a977ffe1263db168b0
Vyacheslav Oleshko <vyacheslav.oleshko.work@gmail.com>
2021-07-04 01:20:22 +0300

    fix mail layout

--- resources/views/emails/companies/company-added.blade.php
+++ resources/views/emails/companies/company-added.blade.php
@@ -1,4 +1,2 @@
 <p>{{ __('email.new') }}</p>
-<p>{{ __('email.check') }}
-    <a href="{{ route('companies.show', ['company' => $company->id]) }}">{{ $company->name }}</a>
-</p>
+<p>{{ __('email.check') }}: <a href="{{ route('companies.show', ['company' => $company->id]) }}">{{ $company->name }}</a></p>
